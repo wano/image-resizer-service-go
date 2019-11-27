df0cacce18d91abef8a3a99f2162dc96ccc57af7
伏見尚人 <fushimi@wano.co.jp>
2019-11-27 10:40:04 +0900

    :art: [Improve] fix  decode option

--- lambda/main.go
+++ lambda/main.go
@@ -10,15 +10,11 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/disintegration/imaging"
 	"github.com/labstack/gommon/log"
-	"github.com/sanity-io/litter"
-	"image"
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
-	"io"
-	"mime"
+	"net/http"
 	"os"
-	"path/filepath"
 )
 
 var BUCKET string
@@ -36,24 +32,48 @@ func serveFunc(request events.APIGatewayProxyRequest) (resp events.APIGatewayPro
 		return
 	}
 
-	s3Sdk :=  s3.New(sess)
+	s3Sdk := s3.New(sess)
 	path := request.Path
 
-	obj  , err := s3Sdk.GetObject(&s3.GetObjectInput{
-		Bucket:                     &BUCKET,
-		Key:                        &path,
+	obj, err := s3Sdk.GetObject(&s3.GetObjectInput{
+		Bucket: &BUCKET,
+		Key:    &path,
 	})
 	if err != nil {
 		log.Error(err)
 		return
 	}
 
+	original := new(bytes.Buffer)
+	_, err = original.ReadFrom(obj.Body)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	mimeAndDecodeType, err := getMimeAndDecodeType(original)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
 	dst := []byte{}
-	mimeType := mime.TypeByExtension(filepath.Ext(path))
+	mimeType := mimeAndDecodeType.ContentType
+
+	const LAMBDA_MAX_RESPONSE = 1024 * 1024 * 5
 
 	params := getParams(request.QueryStringParameters)
 	if params.HasOptions() {
-		dst  , mimeType , err = imagProcess(obj.Body , params)
+		dst, mimeType, err = imagProcess(original, params, *mimeAndDecodeType)
+		if err != nil {
+			return
+		}
+
+	} else if *obj.ContentLength > LAMBDA_MAX_RESPONSE {
+		forceResize := 1920
+		dst, mimeType, err = imagProcess(original, Params{
+			Width: &forceResize,
+		}, *mimeAndDecodeType)
 		if err != nil {
 			return
 		}
@@ -62,58 +82,62 @@ func serveFunc(request events.APIGatewayProxyRequest) (resp events.APIGatewayPro
 		buf := new(bytes.Buffer)
 		_, err = buf.ReadFrom(obj.Body)
 		if err != nil {
+			log.Error(err)
 			return
 		}
 		dst = buf.Bytes()
-
 	}
 
 	sEnc := base64.StdEncoding.EncodeToString(dst)
 
 	resp = events.APIGatewayProxyResponse{
-		StatusCode:        200,
-		Headers:           map[string]string{
-			`Content-Type` : mimeType,
+		StatusCode: 200,
+		Headers: map[string]string{
+			`Content-Type`: mimeType,
 		},
 		//MultiValueHeaders: nil,
-		Body:              sEnc,
-		IsBase64Encoded:   true,
+		Body:            sEnc,
+		IsBase64Encoded: true,
 	}
 
-	return resp , nil
+	return resp, nil
 }
 
-func imagProcess(r io.ReadCloser  , params Params) (encoded []byte , mimeType string , err error) {
+func imagProcess(buf *bytes.Buffer, params Params, md MimeAndDecodeType) (encoded []byte, mimeType string, err error) {
 
-	img, t, err := image.Decode(r)
-	if err != nil {
-		log.Error(err)
-		return nil ,"" ,  err
+	decodeOptions := []imaging.DecodeOption{}
+
+	if params.AutoRotate {
+		decodeOptions = append(decodeOptions, imaging.AutoOrientation(true))
 	}
 
-	decodeType  , mimeType := typeToDecodeAndMime(t)
+	img, err := imaging.Decode(buf, decodeOptions...)
+	if err != nil {
+		log.Error(err)
+		return nil, "", err
+	}
+
+	//decodeType  , mimeType := typeToDecodeAndMime(img.)
 
 	//rectange of image
 	rctSrc := img.Bounds()
 
-	litter.Dump(params)
-
-	w ,  h := func() (int  ,int){
-		if params.Width == nil && params.Height == nil  {
-			return rctSrc.Dx() , rctSrc.Dy()
+	w, h := func() (int, int) {
+		if params.Width == nil && params.Height == nil {
+			return rctSrc.Dx(), rctSrc.Dy()
 		}
 
-		if params.Width != nil && params.Height != nil  {
-			return *params.Width , *params.Height
+		if params.Width != nil && params.Height != nil {
+			return *params.Width, *params.Height
 		}
 
 		if params.Width != nil {
 			ratio := float64(rctSrc.Dy()) / float64(rctSrc.Dx())
-			return *params.Width , int(float64(*params.Width) * ratio)
+			return *params.Width, int(float64(*params.Width) * ratio)
 		}
 
 		ratio := float64(rctSrc.Dx()) / float64(rctSrc.Dy())
-		return int(float64(*params.Height) * ratio)  , *params.Height
+		return int(float64(*params.Height) * ratio), *params.Height
 
 	}()
 
@@ -122,41 +146,43 @@ func imagProcess(r io.ReadCloser  , params Params) (encoded []byte , mimeType st
 
 	//encode resized image
 	/*
-	dst := new(bytes.Buffer)
+		dst := new(bytes.Buffer)
 
-	 */
-	err = imaging.Encode(dst , imgDst , decodeType)
+	*/
+	log.Info(`encode start`)
+	err = imaging.Encode(dst, imgDst, md.Format)
 	if err != nil {
 		log.Error(err)
-		return nil, "",  err
+		return nil, "", err
 	}
-	return dst.Bytes() , mimeType  , nil
+	log.Info(`encode end`)
+	return dst.Bytes(), mimeType, nil
 
 }
 
-func typeToDecodeAndMime(t string) (imaging.Format , string) {
+func typeToDecodeAndMime(t string) (imaging.Format, string) {
 
 	switch t {
 	case "jpeg":
-			return imaging.JPEG , `image/jpeg`
+		return imaging.JPEG, `image/jpeg`
 	case "gif":
-			return imaging.GIF , `image/gif`
+		return imaging.GIF, `image/gif`
 	case "png":
-			return imaging.PNG  ,`image/png`
+		return imaging.PNG, `image/png`
 	case "tiff":
-		return imaging.TIFF  ,`image/tiff`
+		return imaging.TIFF, `image/tiff`
 	default:
-		return imaging.JPEG , `image/jpeg`
+		return imaging.JPEG, `image/jpeg`
 	}
 }
 
-
 type Params struct {
-	Width *int `json:"width,string,omitempty"`
-	Height *int  `json:"height,string,omitempty"`
+	Width      *int `json:"width,string,omitempty"`
+	Height     *int `json:"height,string,omitempty"`
+	AutoRotate bool `json:"auto_rotate,string,omitempty"`
 }
 
-func (self *Params) HasOptions () bool {
+func (self *Params) HasOptions() bool {
 	if self.Width != nil {
 		return true
 	}
@@ -166,14 +192,13 @@ func (self *Params) HasOptions () bool {
 	return false
 }
 
-
-func getParams(m map[string]string)  Params{
+func getParams(m map[string]string) Params {
 	p := Params{}
-	if len(m)  ==0 {
+	if len(m) == 0 {
 		return p
 	}
 
-	j , err := json.Marshal(m)
+	j, err := json.Marshal(m)
 	if err != nil {
 		log.Error(err)
 	}
@@ -184,4 +209,41 @@ func getParams(m map[string]string)  Params{
 	}
 
 	return p
+}
+
+func getMimeAndDecodeType(b *bytes.Buffer) (*MimeAndDecodeType, error) {
+
+	bb, err := b.ReadBytes(512)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	contentType := http.DetectContentType(bb)
+
+	f := func() imaging.Format {
+		switch contentType {
+		case `image/jpeg`:
+			return imaging.JPEG
+		case "image/gif":
+			return imaging.GIF
+		case "image/png":
+			return imaging.PNG
+		case "image/tiff":
+			return imaging.TIFF
+		default:
+			return imaging.JPEG
+		}
+
+	}()
+
+	return &MimeAndDecodeType{
+		ContentType: contentType,
+		Format:      f,
+	}, nil
+}
+
+type MimeAndDecodeType struct {
+	ContentType string
+	Format      imaging.Format
 }
